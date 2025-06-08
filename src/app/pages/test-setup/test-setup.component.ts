ffc0945ef52c36b36dcc945d2f4e32ca3a6b3f63
sourav <sk4118251@gmail.com>
2025-06-08 14:51:47 +0530

    shifted to angular

---
+++ src/app/pages/test-setup/test-setup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-setup',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-setup.component.html',
+  styleUrl: './test-setup.component.css'
+})
+export class TestSetupComponent {
+
+}
